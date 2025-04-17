c6837b9bfecf506e68c74dd6c52780b1eec3ac2c
Scott Howard <scotthowy96@gmail.com>
2025-04-16 21:40:18 -0400

    reverting changes

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { DiscordSDK } from '@discord/embedded-app-sdk';
 
 const discordSdk = new DiscordSDK("1295516437112426586");
+let auth = discordSdk.getAuth();
 // Global promise that resolves when Discord SDK is ready
 window.discordReady = new Promise(async (resolve, reject) => {
     try {
