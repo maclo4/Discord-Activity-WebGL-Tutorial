cbb7317b30f015531f49436af32d62dcfdc86889
Scott Howard <scotthowy96@gmail.com>
2025-04-18 12:58:57 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,33 +1,56 @@
-import { insertCoin, getState, setState, onPlayerJoin, getDiscordClient } from 'playroomkit'
-import { DiscordSDK } from "@discord/embedded-app-sdk";
+import { insertCoin, getState, setState, onPlayerJoin } from 'playroomkit'
+// Show popup and ask user permissions for their discord information
+await insertCoin({
+  gameId: "<id from playroom portal>",
+  discord: true
+});
+ 
+// Print current player's server name and avatar url
+//console.log(me().getProfile())
+ 
+// Players of this activity session are now in same room now! Set shared state
+setState("topScore", 42)
+ 
+// Called for each player joining the same activity session
+onPlayerJoin((playerState)=>{
+  // Print player's Discord name and avatar url
+  console.log(playerState.getProfile()) // {name: "username", photo: "https://cdn.discord...."}
+})
+ 
+// On some other player
+const topScore = getState("topScore")
+console.log("$$$$ top score: " + topScore)
 
-async function startPurchase() {
-    
-    console.log("$$$$ pre getDiscordClient");
-    let discordClient = getDiscordClient();
-    
-    console.log("$$$$ pre purchase");
-    await discordClient.commands.startPurchase({sku_id: "1358991165872930949"});
-    
-    console.log("$$$$ post purchase");
-}
+// import { insertCoin, getState, setState, onPlayerJoin, getDiscordClient } from 'playroomkit'
+// import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-(async () => {
-    console.log("$$$$ YAY yayayayayayayayayayayayayyaysdfojhagsldkjfhasldkjfhl;akj")
-    await insertCoin({
-      gameId: "1295516437112426586",
-      discord: true
-    });
-    console.log("$$$$ coin inserted");
+// async function startPurchase() {
+    
+//     console.log("$$$$ pre getDiscordClient");
+//     let discordClient = getDiscordClient();
+    
+//     console.log("$$$$ pre purchase");
+//     await discordClient.commands.startPurchase({sku_id: "1358991165872930949"});
+    
+//     console.log("$$$$ post purchase");
+// }
+
+// (async () => {
+//     console.log("$$$$ YAY yayayayayayayayayayayayayyaysdfojhagsldkjfhasldkjfhl;akj")
+//     await insertCoin({
+//       gameId: "1295516437112426586",
+//       discord: true
+//     });
+//     console.log("$$$$ coin inserted");
   
-    setState("topScore", 42);
+//     setState("topScore", 42);
   
-    onPlayerJoin((playerState) => {
-      console.log(playerState.getProfile());
-    });
+//     onPlayerJoin((playerState) => {
+//       console.log(playerState.getProfile());
+//     });
   
-    const topScore = getState("topScore");
+//     const topScore = getState("topScore");
   
-    console.log("$$$$ top score: " + topScore);
-    await startPurchase();
-  })();
+//     console.log("$$$$ top score: " + topScore);
+//     await startPurchase();
+//   })();
