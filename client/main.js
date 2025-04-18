9c30962b4d88f18a617b7633fb889f076989b7b0
Scott Howard <scotthowy96@gmail.com>
2025-04-18 12:40:43 -0400

    Update main.js

--- client/main.js
+++ client/main.js
@@ -2,17 +2,23 @@ import { insertCoin, getState, setState, onPlayerJoin, getDiscordClient } from '
 import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 async function startPurchase() {
+    
+    console.log("$$$$ pre getDiscordClient");
     let discordClient = getDiscordClient();
+    
+    console.log("$$$$ pre purchase");
     await discordClient.commands.startPurchase({sku_id: "1358991165872930949"});
+    
+    console.log("$$$$ post purchase");
 }
 
 (async () => {
+    console.log("$$$$ YAY yayayayayayayayayayayayayyaysdfojhagsldkjfhasldkjfhl;akj")
     await insertCoin({
-      gameId: "",
+      gameId: "1295516437112426586",
       discord: true
     });
-  
-    console.log(me().getProfile());
+    console.log("$$$$ coin inserted");
   
     setState("topScore", 42);
   
@@ -22,115 +28,6 @@ async function startPurchase() {
   
     const topScore = getState("topScore");
   
+    console.log("$$$$ top score: " + topScore);
     await startPurchase();
   })();
-
-
-// import { DiscordSDK } from '@discord/embedded-app-sdk';
-
-// const discordSdk = new DiscordSDK("1295516437112426586");
-// //let auth = discordSdk.getAuth();
-// // Global promise that resolves when Discord SDK is ready
-// window.discordReady = new Promise(async (resolve, reject) => {
-//     try {
-//       await setupDiscordSdk();
-//       window.discord = discordSdk;
-//       resolve(); // let anything else know it's ready
-//     } catch (e) {
-//       reject(e);
-//     }
-//   });
-  
-// // if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-// //    // Mobile device style: fill the whole browser client area with the game canvas:
-// //    var meta = document.createElement('meta');
-// //    meta.name = 'viewport';
-// //    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-// //    document.getElementsByTagName('head')[0].appendChild(meta);
-
-// //    var canvas = document.querySelector("#unity-canvas");
-// //    canvas.style.width = "100%";
-// //    canvas.style.height = "100%";
-// //    canvas.style.position = "fixed";
-
-// //    document.body.style.textAlign = "left";
-// //  }
-
-// //  createUnityInstance(document.querySelector("#unity-canvas"), {
-// //    dataUrl: "Build/client.data",
-// //    frameworkUrl: "Build/client.framework.js",
-// //    codeUrl: "Build/client.wasm",
-// //    streamingAssetsUrl: "StreamingAssets",
-// //    companyName: "Maclo4",
-// //    productName: "DugDig",
-// //    productVersion: "1.0",
-// //    // matchWebGLToCanvasSize: false, // Uncomment this to separately control WebGL canvas render size and DOM element size.
-// //    // devicePixelRatio: 1, // Uncomment this to override low DPI rendering on high DPI displays.
-// //  }).then(async unityInstance => {
-// //    await setupDiscordSdk();
-
-// //    const member  = await fetch(`https://discord.com/api/v10/users/@me/guilds/${discordSdk.guildId}/member`, {
-// //       headers: {
-// //          // NOTE: we're using the access_token provided by the "authenticate" command
-// //          Authorization: `Bearer ${auth.access_token}`,
-// //          'Content-Type': 'application/json',
-// //       },
-// //    }).then((response) => response.json());
-
-// //    let username = member?.nick ?? auth.user.global_name;
-// //    let iconUrl = "";
-
-// //    if (member?.avatar != null) {
-// //       iconUrl = `https://cdn.discordapp.com/guilds/${discordSdk.guildId}/users/${auth.user.id}/avatars/${member.avatar}.png?size=${256}`;
-// //    } else {
-// //       iconUrl = `https://cdn.discordapp.com/avatars/${auth.user.id}/${auth.user.avatar}.png?size=${256}`;
-// //    }
-
-// //    if (unityInstance) {
-// //       unityInstance.SendMessage("Bridge", "SetUserData", JSON.stringify({
-// //          "username": username,
-// //          "iconUrl": iconUrl,
-// //       }))
-// //    }
-
-// // });
-
-// async function setupDiscordSdk(){
-//    await discordSdk.ready();
-
-//    // Authorize with Discord Client
-//    const { code } = await discordSdk.commands.authorize({
-//       client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-//       response_type: "code",
-//       state: "",
-//       prompt: "none",
-//       scope: [
-//          "identify",
-//          "guilds",
-//          "guilds.members.read"
-//       ],
-//    });
-
-//    console.log(code)
-
-//    // Retrieve an access_token from your activity's server
-//    const response = await fetch("/server/token", {
-//       method: "POST",
-//       headers: {
-//          "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//          code,
-//       }),
-//    });
-//    const { access_token } = await response.json();
-
-//    // Authenticate with Discord client (using the access_token)
-//    auth = await discordSdk.commands.authenticate({
-//       access_token,
-//    });
-
-//    if (auth == null) {
-//       throw new Error("Authenticate command failed");
-//    }
-// }
