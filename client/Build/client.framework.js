a9a9dc370015bdb55e23a0966c21ffb1320fea96
Scott Howard <scotthowy96@gmail.com>
2025-06-22 17:27:57 -0400

    fix items???? (crudely style)

--- client/Build/client.framework.js
+++ client/Build/client.framework.js
@@ -48127,10 +48127,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7633344: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7633399: ($0) => { performance.now = function() { return $0; }; },  
- 7633447: ($0) => { performance.now = function() { return $0; }; },  
- 7633495: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7633376: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7633431: ($0) => { performance.now = function() { return $0; }; },  
+ 7633479: ($0) => { performance.now = function() { return $0; }; },  
+ 7633527: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
