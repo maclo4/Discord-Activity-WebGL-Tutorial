7e7f38e7a161c2b34ccd8e755f7a2a552bff32df
Scott Howard <scotthowy96@gmail.com>
2025-06-22 16:14:02 -0400

    fix online deletion logic?

--- client/Build/client.loader.js
+++ client/Build/client.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95997287,
+    wasmFileSize: 95997140,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
