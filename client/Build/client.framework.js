4cc1dc15e5d467a08bf87ff15426c8d5eb4fa58f
Scott Howard <scotthowy96@gmail.com>
2025-06-28 02:47:12 -0400

    camera changes

--- client/Build/client.framework.js
+++ client/Build/client.framework.js
@@ -48127,10 +48127,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7633472: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7633527: ($0) => { performance.now = function() { return $0; }; },  
- 7633575: ($0) => { performance.now = function() { return $0; }; },  
- 7633623: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7633504: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7633559: ($0) => { performance.now = function() { return $0; }; },  
+ 7633607: ($0) => { performance.now = function() { return $0; }; },  
+ 7633655: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- client/Build/client.loader.js
+++ client/Build/client.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95998039,
+    wasmFileSize: 95999046,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
