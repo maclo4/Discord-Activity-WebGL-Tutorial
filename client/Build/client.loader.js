d6a55909d0d26cf53184da6b0ed619c60478685f
Scott Howard <scotthowy96@gmail.com>
2025-07-06 18:03:40 -0400

    solo + mud fix

--- client/Build/client.loader.js
+++ client/Build/client.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 96015649,
+    wasmFileSize: 95776163,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
