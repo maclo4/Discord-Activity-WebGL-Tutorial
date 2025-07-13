1f3c1c31007ecc2a75290ec1b24fb2c56a986449
Scott Howard <scotthowy96@gmail.com>
2025-07-13 16:26:08 -0400

    fix missiles???

--- client/Build/client.loader.js
+++ client/Build/client.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 96081354,
+    wasmFileSize: 95839618,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
